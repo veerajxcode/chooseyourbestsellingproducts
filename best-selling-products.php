5b24e9f1bab732454857adf26b8dc1f2b5e85df6
veerajxcode <veeraj11yadav@gmail.com>
2024-07-09 18:04:23 +0530

    initial setup of plugin done

---
+++ best-selling-products.php
@@ -0,0 +1,15 @@
+/*
+ * Plugin Name:       Best Selling Products
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       Allow you to show best selling products of your store
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Veeraj Yadav
+ * Author URI:        https://author.example.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       bestselpro
+ * Domain Path:       /languages
+ * Requires Plugins:  woocommerce
+ */
